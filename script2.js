0130485a0b54cb00054202fc60eb788ea3496de3
elinanomade <elinanomad@gmail.com>
2022-11-15 15:25:17 +0100

    rotate

--- script2.js
+++ script2.js
@@ -1,10 +1,10 @@
 'use script';
 
-const circle = document.getElementById("circle");
+/* const circle = document.getElementById("circle");
 circle.addEventListener("click", changeColor);
 function changeColor() {
     circle.classList.toggle("circleRed");
-}
+}*/
 
 
 /*
@@ -14,11 +14,13 @@ function rotateCircle() {
     circle.classList.toggle("rotateCircle");
 } */
 
-/*
-const cbox = document.querySelectorAll(".all");
-cbox.addEventListener("click", rotateCircle)
-function rotateCircle() {
-    circle.classList.toggle("rotateCircle");
+const circles = document.querySelectorAll(".circle");
 
+
+ for (let i = 0; i <12; i++) {
+    circles[i].addEventListener("click", rotateCircle);
+    function rotateCircle() {
+    circles[i].classList.toggle("rotateCircle");
+    
  }
- */
+}
